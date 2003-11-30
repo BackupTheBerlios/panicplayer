e0de2761139926f4ece75c06e885e26ffbf20c7b
schurli <schurli>
2003-11-30 18:02:35 +0000

    optionPane added

--- panicplayer/src/b12/panik/ui/PanicPlayer.java
+++ panicplayer/src/b12/panik/ui/PanicPlayer.java
@@ -1,7 +1,7 @@
 // ----------------------------------------------------------------------------
 // [b12] Java Source File: PanicPlayer
 //                created: 26.10.2003
-//              $Revision: 1.7 $
+//              $Revision: 1.8 $
 // ----------------------------------------------------------------------------
 package b12.panik.ui;
 
@@ -16,10 +16,13 @@ import java.beans.PropertyChangeListener;
 import java.io.File;
 
 import javax.media.Player;
+import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JMenuBar;
+import javax.swing.JOptionPane;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.WindowConstants;
 
 import b12.panik.io.MediaIOException;
 import b12.panik.io.MediaInput;
@@ -32,36 +35,38 @@ import b12.panik.util.Logging;
 public class PanicPlayer extends JFrame {
 
     private static final boolean CLOSE_PLAYER_ON_EXIT = true;
-    
+
     private PanicAudioPlayer panicAudioPlayer;
     private PlayerControlPanel panelPlayerControl;
 
     MediaInput input = new MediaInput();
-    
+
     private JMenuBar menuBar;
     private FileMenu fileMenu;
 
     /** Creates a new PanicPlayer. */
     public PanicPlayer() {
         super("Panic Player");
-		try {
-			String plaf = "com.stefankrause.xplookandfeel.XPLookAndFeel";
-			//String plaf = "com.sun.java.swing.plaf.windows.WindowsLookAndFeel";
-			UIManager.setLookAndFeel(plaf);
-		} catch (UnsupportedLookAndFeelException e) {
-			String errorCause = "Unsupported Look and Feel";
-			Logging.info(errorCause);
-		} catch (ClassNotFoundException e) {
-			String errorCause = "Look and Feel class not found";
-			Logging.info(errorCause);
-		} catch (InstantiationException e) {
-			String errorCause = "Look and Feel InstantiationException";
-			Logging.info(errorCause);
-		} catch (IllegalAccessException e) {
-			String errorCause = "Look and Feel Illegal Acces";
-			Logging.info(errorCause);
-		}
-    
+        try {
+            //String plaf = "com.sun.java.swing.plaf.gtk.GTKLookAndFeel";
+            //String plaf =
+            // "com.sun.java.swing.plaf.windows.WindowsLookAndFeel";
+            //UIManager.setLookAndFeel(plaf);
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (UnsupportedLookAndFeelException e) {
+            String errorCause = "Unsupported Look and Feel";
+            Logging.info(errorCause);
+        } catch (ClassNotFoundException e) {
+            String errorCause = "Look and Feel class not found";
+            Logging.info(errorCause);
+        } catch (InstantiationException e) {
+            String errorCause = "Look and Feel InstantiationException";
+            Logging.info(errorCause);
+        } catch (IllegalAccessException e) {
+            String errorCause = "Look and Feel Illegal Acces";
+            Logging.info(errorCause);
+        }
+        setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
         addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent e) {
                 exitApplication();
@@ -92,7 +97,6 @@ public class PanicPlayer extends JFrame {
 
     }
 
-
     protected void loadSoundFile(File f) {
         Player player;
         try {
@@ -112,14 +116,27 @@ public class PanicPlayer extends JFrame {
 
     /** Exits from the application */
     protected void exitApplication() {
-        if (CLOSE_PLAYER_ON_EXIT) {
-            if (panicAudioPlayer != null) {
-                panicAudioPlayer.stop();
+        final JDialog wnd = new JDialog();
+
+        int ret =
+            (int) JOptionPane.showConfirmDialog(
+                wnd,
+                "Do you really want to exit",
+                "Quit",
+                JOptionPane.YES_NO_OPTION,
+                JOptionPane.QUESTION_MESSAGE,
+                null);
+        if (ret == JOptionPane.YES_OPTION) {
+            if (CLOSE_PLAYER_ON_EXIT) {
+                if (panicAudioPlayer != null) {
+                    panicAudioPlayer.stop();
+                }
             }
+
+            setVisible(false);
+            dispose();
+            System.exit(0);
         }
-        setVisible(false);
-        dispose();
-        System.exit(0);
     }
 
     /** Shows the player. */
@@ -139,7 +156,6 @@ public class PanicPlayer extends JFrame {
         if (frameSize.width > screenSize.width) {
             frameSize.width = screenSize.width;
         }
-        setLocation((screenSize.width - frameSize.width) / 2,
-                (screenSize.height - frameSize.height) / 2);
+        setLocation((screenSize.width - frameSize.width) / 2, (screenSize.height - frameSize.height) / 2);
     }
 }
